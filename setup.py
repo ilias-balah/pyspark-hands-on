0a13317852e114e6d7030abfbd970b00bb9d5fe3
Ilias Balah <balah.ilias@gmail.com>
2025-05-20 20:07:15 -0400

    Package the project for reusable imports and development mode setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from os.path import dirname, join, isfile
+from setuptools import setup, find_packages
+
+def read_requirements():
+    """
+    Reads the requirements.txt file and returns a list of required packages.
+    """
+    # Read the requirements from the requirements.txt file
+    req_path = join(dirname(__file__), 'requirements.txt')
+
+    # Check if the file exists
+    if not isfile(req_path):
+        reqs = []
+    
+    # Read the requirements from the file
+    with open('requirements.txt', 'r+') as f:
+        reqs = [ req.strip() for req in f if req.strip() and not req.startswith('#') ]
+
+    print(reqs)
+    return reqs
+
+setup(
+    name='spark-handson',
+    version='0.1',
+    author='Ilias',
+    packages=find_packages(),
+    install_requires=read_requirements()
+)
